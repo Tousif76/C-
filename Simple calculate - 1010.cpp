3387ff4b23bcc66539d1b153875d4f8e34d191c7
Tousif76 <110369882+Tousif76@users.noreply.github.com>
2022-08-05 10:43:31 +0600

    Add files via upload

---
+++ Simple calculate - 1010.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int p1, u1, p2, u2;
+    double pr1, pr2;
+    cin >>p1>>u1>>pr1>>p2>>u2>>pr2;
+
+    double pay = (u1*pr1)+(u2*pr2);
+    cout <<fixed<< setprecision(2)<< "VALOR A PAGAR: R$ " <<pay<< endl;
+
+    return 0;
+
+}
