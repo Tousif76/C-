25b56538c7f4a4f5b5da4ed47fef5fcd7e61c21d
Tousif76 <110369882+Tousif76@users.noreply.github.com>
2022-08-07 19:23:27 +0600

    Add files via upload

---
+++ Sphere - 1011.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int r;
+    cin >>r;
+
+    double result = (4.0/3*3.14159*r*r*r);
+    cout <<"VOLUME = "<<result<<fixed<<setprecision (3)<< endl;
+
+    return 0;
+
+
+}
