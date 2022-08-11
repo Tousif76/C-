d09831ff93608c076dade5bef0b243b72cba1273
Tousif76 <110369882+Tousif76@users.noreply.github.com>
2022-08-11 19:56:53 +0600

    Add files via upload

---
+++ Spent fuels - 1017.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+   int h,k;
+   cin >>h>>k;
+
+   double TL = (h*k)/12.0;
+   cout<<fixed<<setprecision(3)<<TL<<endl;
+   return 0;
+
+
+}
