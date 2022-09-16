0a72a9a932c6ead6c3a977ee2fe50b273d57499f
Tousif76 <110369882+Tousif76@users.noreply.github.com>
2022-09-16 22:43:42 +0600

    Add files via upload

---
+++ EOPN - 1066.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int even=0,odd=0,positive=0,negative=0,i,num;
+    for (i=0; i<5; i++)
+    {
+        cin>>num;
+        if(num%2==0)
+        {
+            even++;
+        }
+        if(num%2!=0)
+        {
+            odd++;
+        }
+        if(num>0)
+        {
+            positive++;
+        }
+        if(num<0)
+        {
+            negative++;
+        }
+    }
+
+    cout<<even<<" valor(es) par(es)"<<endl;
+    cout<<odd<<" valor(es) impar(es)"<<endl;
+    cout<<positive<<" valor(es) positivo(s)"<<endl;
+    cout<<negative<<" valor(es) negativo(s)"<<endl;
+
+    return 0;
+}
