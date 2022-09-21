041fce7cd41eefc213f5fbf17fd4a6e589c532d8
Tousif76 <110369882+Tousif76@users.noreply.github.com>
2022-09-21 19:08:21 +0600

    Add files via upload

---
+++ Asc Dsc - 1113.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int  x,y,aux;
+    while (true)
+    {
+        cin>>x;
+        cin>>y;
+
+        if(x==y) break;
+
+        if(x>y)
+        {
+            cout <<"Decrescente"<<endl;
+        }
+        else
+        {
+            cout <<"Crescente"<<endl;
+        }
+
+
+    }
+
+    return 0;
+
+}
